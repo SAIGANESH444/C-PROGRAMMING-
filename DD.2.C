9910f53e966641f3aa0f55a391c15459e10c01f1
J.SAI GANESH <98070964+SAIGANESH444@users.noreply.github.com>
2022-02-03 06:41:02 +0530

    Create DD.2.C

---
+++ DD.2.C
@@ -0,0 +1,24 @@
+// TOTAL OF THE ROW
+
+ #include<stdio.h>
+
+ void main()
+ {
+ int arr[5][5];
+ int i,j,total;
+
+  srand(time(0));
+  for(i=0;i<5;i++)
+  {
+      total=0;
+      for(j=0;j<5;j++)
+
+      {
+       arr[i][j]=rand()%50;
+    printf("%5d",arr[i][j]);
+    total+=arr[i][j];
+      }
+    printf("%5d\n",total);
+
+  }
+ }
